83c926961cd031c4f126863ee1afaf43747996a5
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-11-22 15:37:51 -0800

    added node modules

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const questions = [
     type: "input",
     name: "name",
     message: "What is your name?",
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log ('Please enter your name')
+        return false;
+
+      }
+    }
 
   }
 ]
